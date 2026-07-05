74cbc631d0ec1a95ec4b0a803f7b056367b56bb3
Pedro Moura <henriquepedro3223@proton.me>
2026-07-04 21:33:56 -0300

    Adicionado arquivo para import das imagens

---
+++ src/exercises/importImages.js
@@ -0,0 +1,13 @@
+import supinoReto from './supino-reto.webp';
+/*import tricepsCorda from './triceps-corda.webp';
+import puxadaFrontal from './puxada-frontal.webp';
+import roscaDireta from './rosca-direta.webp';
+import agachamento from './agachamento.webp';*/
+
+export const exerciseImages = {
+  supinoReto/*,
+  tricepsCorda,
+  puxadaFrontal,
+  roscaDireta,
+  agachamento,*/
+};
